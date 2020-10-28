2b64019286b6318dd626fc87c2db6d9993362fad
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-10-28 12:51:36 -0300

    feat: add component loader

---
+++ components/loader/loader.component.ts
@@ -0,0 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+
+import { LoaderService } from './loader.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
+      transition(':leave', [animate(300, style({ opacity: 0 }))]),
+    ]),
+  ],
+})
+export class LoaderComponent {
+  constructor(public loaderService: LoaderService) {}
+}
