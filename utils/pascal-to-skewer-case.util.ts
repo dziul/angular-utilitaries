cddc09d3d865845e5213a65105c3234c268f18da
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2021-05-11 17:30:59 -0300

    fix: correcao filter

--- utils/pascal-to-skewer-case.util.ts
+++ utils/pascal-to-skewer-case.util.ts
@@ -1,32 +1,32 @@
 interface Options {
 	ignoreWords: string[];
-	lowerCase: boolean;
+	forceLowerCase: boolean;
 }
 type OptionsPartial = Partial<Options>;
 
 const optionsIntial: Options = {
 	ignoreWords: [],
-	lowerCase: true
+	forceLowerCase: true
 };
 
 const SEPARETOR_SKEWER = '-';
+const toLowerCase = (text: string) => text.toLowerCase();
 
-const convert = (pascalCaseText: string, lowerCase: boolean) => {
+const convert = (pascalCaseText: string, forceLowerCase: boolean) => {
 	const formated = pascalCaseText.replace(/([a-z])([A-Z])/g, `$1${SEPARETOR_SKEWER}$2`);
 
-	return lowerCase ? formated.toLowerCase() : formated;
+	return forceLowerCase ? toLowerCase(formated) : formated;
 };
-
-const toLowerCase = (text: string) => text.toLowerCase();
 
 const filterWords = (words: string[], removeWords: string[]) => {
 	const removeWordsLower = removeWords.map(toLowerCase);
-	return words.filter(word => removeWordsLower.includes(toLowerCase(word)));
+
+	return words.filter(word => !removeWordsLower.includes(toLowerCase(word)));
 };
 
 export const pascalCaseToWordList = (pascalCaseText: string, options: OptionsPartial = {}): string[] => {
 	const optionsUpdated = { ...optionsIntial, ...options };
-	const words = convert(pascalCaseText, optionsUpdated.lowerCase).split(SEPARETOR_SKEWER);
+	const words = convert(pascalCaseText, optionsUpdated.forceLowerCase).split(SEPARETOR_SKEWER);
 
 	return filterWords(words, optionsUpdated.ignoreWords);
 };
